42589fd290ae85370b2e922a504e55d355677026
unknown <hudahaksonoug@gmail.com>
2022-05-26 13:18:27 +0700

    Hudha

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -104,55 +104,42 @@
     </div>
 </section>
 
-<section class="ftco-section img" id="ftco-testimony" style="background-image: url(images/bg_1.jpg);" data-stellar-background-ratio="0.5">
+<section class="ftco-section img" id="ftco-testimony" style="background-image: url(images/savana/savana2.jpg);" data-stellar-background-ratio="0.5">
     <div class="overlay"></div>
     <div class="container">
         <div class="row justify-content-center mb-5">
             <div class="col-md-7 heading-section text-center ftco-animate">
                 <span class="subheading">Testimony</span>
                 <h2 class="mb-4">Customers Says</h2>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                <!-- <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p> -->
             </div>
         </div>
     </div>
     <div class="container-wrap">
         <div class="row d-flex no-gutters">
-            <div class="col-lg align-self-sm-end ftco-animate">
-                <div class="testimony">
-                    <blockquote>
-                        <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small.&rdquo;</p>
-                    </blockquote>
-                    <div class="author d-flex mt-4">
-                        <div class="image mr-3 align-self-center">
-                            <img src="images/person_1.jpg" alt="">
-                        </div>
-                        <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
-                    </div>
-                </div>
-            </div>
             <div class="col-lg align-self-sm-end">
                 <div class="testimony overlay">
                     <blockquote>
-                        <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.&rdquo;</p>
+                        <p>&ldquo;Sungguh pelayanan yang luarbiasa, semua dekorasi yang disajikan sangan rapih dan detail seperti pada produk yang tertera pada gambar.&rdquo;</p>
                     </blockquote>
                     <div class="author d-flex mt-4">
                         <div class="image mr-3 align-self-center">
-                            <img src="images/person_2.jpg" alt="">
+                            <img src="images/web/user-men.png" alt="">
                         </div>
-                        <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
+                        <div class="name align-self-center">Nur Hudha Haksono <span class="position">Customer Paket Intimate</span></div>
                     </div>
                 </div>
             </div>
             <div class="col-lg align-self-sm-end ftco-animate">
                 <div class="testimony">
                     <blockquote>
-                        <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name. &rdquo;</p>
+                        <p>&ldquo;Responnya Cepat Adminnya Ramah dan Pelayanannya Bagus &rdquo;</p>
                     </blockquote>
                     <div class="author d-flex mt-4">
                         <div class="image mr-3 align-self-center">
-                            <img src="images/person_3.jpg" alt="">
+                            <img src="images/web/user-men.png" alt="">
                         </div>
-                        <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
+                        <div class="name align-self-center">Dini Restian <span class="position">Customer Paket Wedding Outdoor</span></div>
                     </div>
                 </div>
             </div>
@@ -163,7 +150,7 @@
                     </blockquote>
                     <div class="author d-flex mt-4">
                         <div class="image mr-3 align-self-center">
-                            <img src="images/person_2.jpg" alt="">
+                            <img src="images/web/user-men.png" alt="">
                         </div>
                         <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
                     </div>
@@ -176,7 +163,7 @@
                     </blockquote>
                     <div class="author d-flex mt-4">
                         <div class="image mr-3 align-self-center">
-                            <img src="images/person_3.jpg" alt="">
+                            <img src="images/web/user-women.png" alt="">
                         </div>
                         <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
                     </div>
--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,72 +1,38 @@
 @section('title', 'Home')
 @include('layouts.navbar')
-<section class="home-slider owl-carousel">
-    <!-- <div class="slider-item" style="background-image: url(images/savana/savana1.jpg);">
-        <div class="overlay"></div>
-        <div class="container">
-            <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
-
-                <div class="col-md-8 col-sm-12 text-center ftco-animate">
-                    <span class="subheading">Welcome</span>
-                    <h1 class="mb-4">SAVANNA DECORATION</h1>
-                    <p class="mb-4 mb-md-5">Ganti Kata Kata Lu bays</p>
-                    <p><a href="#" class="btn btn-primary p-3 px-xl-4 py-xl-3">Order Now</a> <a href="#" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Product</a></p>
-                </div>
-
+<section class="ftco-section img" id="banner" style="background:black" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row justify-content-center mb-5 mt-5">
+            <div class="col-md-7 heading-section text-center ftco-animate">
+                <span class="subheading">Welcome</span>
+                <br>
+                <h2 class="mb-4">Savanna Decoration</h2>
+                <!-- <p style="color:black;">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p> -->
+                <p><a href="/product" class="btn btn-primary p-3 px-xl-4 py-xl-3">View Product</a></p>
             </div>
         </div>
-    </div> -->
+    </div>
+</section>
+<!-- <section class="home-slider owl-carousel">
     @foreach ($banner as $bann)
     <div class="slider-item" style="background-image: url(banner/{{ $bann->file_name }});">
         <div class="overlay"></div>
         <div class="container">
             <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
-
                 <div class="col-md-8 col-sm-12 text-center ftco-animate">
                     <span class="subheading">Welcome</span>
                     <h1 class="mb-4">SAVANNA DECORATION</h1>
                     <p class="mb-4 mb-md-5">Ganti Kata Kata Lu bays</p>
-                    <p><a href="#" class="btn btn-primary p-3 px-xl-4 py-xl-3">Order Now</a> <a href="#" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Product</a></p>
+                    <p><a href="#" class="btn btn-primary p-3 px-xl-4 py-xl-3">Order Now</a><a href="#start" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Product</a></p>
                 </div>
-
             </div>
         </div>
     </div>
     @endforeach
-    <!-- <div class="slider-item" style="background-image: url(images/savana/savana2.jpg);">
-        <div class="overlay"></div>
-        <div class="container">
-            <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
+</section> -->
 
-                <div class="col-md-8 col-sm-12 text-center ftco-animate">
-                    <span class="subheading">Welcome</span>
-                    <h1 class="mb-4">SAVANNA DECORATION</h1>
-                    <p class="mb-4 mb-md-5">Ganti Kata Kata Lu bays</p>
-                    <p><a href="#" class="btn btn-primary p-3 px-xl-4 py-xl-3">Order Now</a> <a href="#" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Product</a></p>
-                </div>
-
-            </div>
-        </div>
-    </div>
-
-    <div class="slider-item" style="background-image: url(images/savana/savana3.jpg);">
-        <div class="overlay"></div>
-        <div class="container">
-            <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
-
-                <div class="col-md-8 col-sm-12 text-center ftco-animate">
-                    <span class="subheading">Welcome</span>
-                    <h1 class="mb-4">SAVANNA DECORATION</h1>
-                    <p class="mb-4 mb-md-5">Ganti Kata Kata Lu bays</p>
-                    <p><a href="#" class="btn btn-primary p-3 px-xl-4 py-xl-3">Order Now</a> <a href="#" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Product</a></p>
-                </div>
-
-            </div>
-        </div>
-    </div> -->
-</section>
-
-<section class="ftco-intro">
+<section id="start" class="ftco-intro">
     <div class="container-wrap">
         <div class="wrap d-md-flex align-items-xl-end">
             <div class="info">
@@ -293,48 +259,35 @@
             <div class="col-md-7 heading-section text-center ftco-animate">
                 <span class="subheading">Testimony</span>
                 <h2 class="mb-4">Customers Says</h2>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                <!-- <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p> -->
             </div>
         </div>
     </div>
     <div class="container-wrap">
         <div class="row d-flex no-gutters">
-            <div class="col-lg align-self-sm-end ftco-animate">
-                <div class="testimony">
-                    <blockquote>
-                        <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small.&rdquo;</p>
-                    </blockquote>
-                    <div class="author d-flex mt-4">
-                        <div class="image mr-3 align-self-center">
-                            <img src="images/person_1.jpg" alt="">
-                        </div>
-                        <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
-                    </div>
-                </div>
-            </div>
             <div class="col-lg align-self-sm-end">
                 <div class="testimony overlay">
                     <blockquote>
-                        <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.&rdquo;</p>
+                        <p>&ldquo;Sungguh pelayanan yang luarbiasa, semua dekorasi yang disajikan sangan rapih dan detail seperti pada produk yang tertera pada gambar.&rdquo;</p>
                     </blockquote>
                     <div class="author d-flex mt-4">
                         <div class="image mr-3 align-self-center">
-                            <img src="images/person_2.jpg" alt="">
+                            <img src="images/web/user-men.png" alt="">
                         </div>
-                        <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
+                        <div class="name align-self-center">Nur Hudha Haksono <span class="position">Customer Paket Intimate</span></div>
                     </div>
                 </div>
             </div>
             <div class="col-lg align-self-sm-end ftco-animate">
                 <div class="testimony">
                     <blockquote>
-                        <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name. &rdquo;</p>
+                        <p>&ldquo;Responnya Cepat Adminnya Ramah dan Pelayanannya Bagus &rdquo;</p>
                     </blockquote>
                     <div class="author d-flex mt-4">
                         <div class="image mr-3 align-self-center">
-                            <img src="images/person_3.jpg" alt="">
+                            <img src="images/web/user-men.png" alt="">
                         </div>
-                        <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
+                        <div class="name align-self-center">Dini Restian <span class="position">Customer Paket Wedding Outdoor</span></div>
                     </div>
                 </div>
             </div>
@@ -345,7 +298,7 @@
                     </blockquote>
                     <div class="author d-flex mt-4">
                         <div class="image mr-3 align-self-center">
-                            <img src="images/person_2.jpg" alt="">
+                            <img src="images/web/user-men.png" alt="">
                         </div>
                         <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
                     </div>
@@ -358,7 +311,7 @@
                     </blockquote>
                     <div class="author d-flex mt-4">
                         <div class="image mr-3 align-self-center">
-                            <img src="images/person_3.jpg" alt="">
+                            <img src="images/web/user-women.png" alt="">
                         </div>
                         <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
                     </div>
